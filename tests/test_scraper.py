35d15e8306605f512bf29ccd487e8af9bb5f2ee2
LeonMilosevic <leonleon787@yahoo.com>
2021-01-11 09:19:56 +0200

    updated tests

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -22,13 +22,12 @@ def test_check_brandIdError():
   with pytest.raises(ValueError):
       scraper.check_brandId('bad')
 
-def calculate_number_of_pages():
+def test_calculate_number_of_pages():
     result = scraper.calculate_number_of_pages(48)
 
     assert result == 1
 
-# needs updating
-def get_phones_url():
+def test_get_phones_url():
     result = scraper.get_phones_url(
         number_of_pages=1,
         brand="Apple",
@@ -36,4 +35,24 @@ def get_phones_url():
         user_agent=UserAgent()
     )
 
-    assert result == list
+    assert len(result) == 42
+
+def test_create_datafrmae():
+  brand = "Apple"
+  phone_price = ['123', '456', '789']
+  phone_model = ['galaxy', 'iphone 7', 'iphone 8']
+  phone_ram = ['3 gb', '4gb', '5gb']
+  phone_storage = ['64gb', '128gb', '16gb']
+  phone_processor = ['some processor', 'some processor2', 'some processor3']
+  phone_camera = ['16', '8', '12']
+
+  result = scraper.create_datafrmae(
+    brand, 
+    phone_price, 
+    phone_model, 
+    phone_ram, 
+    phone_storage, 
+    phone_processor, 
+    phone_camera)
+
+    assert type(result) == pd.DataFrame
